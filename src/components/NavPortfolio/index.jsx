2254d3ea73ba699b307b7112fbcc7548c0a1fdf3
gustavo barretto <gustavobarretto@gmail.com>
2021-11-12 16:34:48 -0300

    organizing the project

--- src/components/NavPortfolio/index.jsx
+++ src/components/NavPortfolio/index.jsx
@@ -1,15 +1,11 @@
 import Navbar from 'react-bootstrap/Navbar';
-import '../NavPortfolio/style.css';
+import '../NavPortfolio/style.scss';
 
 export const NavPortfolio = () => {
     return (
         <Navbar>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
               <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
@@ -25,7 +21,7 @@ export const NavPortfolio = () => {
                       <a className="nav-link active" href="#">projects</a>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link active" href="#">contactt</a>
+                      <a className="nav-link active" href="#">contact</a>
                     </li>  
                   </ul>
                 </div>

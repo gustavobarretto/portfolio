1c0e7037187886b6b1a51eeeab276b99db31ce5b
gustavo barretto <gustavobarretto@gmail.com>
2021-11-16 00:51:24 -0300

    tools concluded. next: portfolio and contacts

--- src/components/Tools/index.jsx
+++ src/components/Tools/index.jsx
@@ -1,26 +1,15 @@
 import './style.scss';
+import javascript from '../../img/icons/javascript.svg'
+import react from '../../img/icons/react.svg'
+import java from '../../img/icons/java.svg'
+import spring from '../../img/icons/spring-framework-icon.svg'
+import python from '../../img/icons/python.svg'
+import typescript from '../../img/icons/typescript.svg'
+import postegresql from '../../img/icons/postgresql.svg'
+import s3 from '../../img/icons/s3.svg'
+
 
 export const Tools = () => {
-    const iconList = {
-        iconConstructor: iconConstructor = (iconId, iconName, iconFileName) => {
-            return {
-                id: iconId,
-                name: iconName,
-                path: '../../img/icons/' + iconFileName
-            }
-        },
-        iconList: [
-            iconConstructor(1, 'Javascript', 'javascript'),
-            iconConstructor(2, 'ReactJS', 'react'),
-            iconConstructor(3, 'Java', 'java'),
-            iconConstructor(4, 'Spring', 'spring-logo'),
-            iconConstructor(5, 'Python', 'python'),
-            iconConstructor(6, 'Typescript', 'typescript'),
-            iconConstructor(7, 'MySQL & PostgreSQL', 'potgresql'),
-            iconConstructor(8, 'AWS S3', 's3'),
-        ]
-    }
-
     return (
         <>
         <section className="page-section" id="services">
@@ -30,56 +19,56 @@ export const Tools = () => {
                 <div className="row gx-4 gx-lg-5">
                     <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
-                            <div className="mb-2"><img src={iconList[0]['path']} alt="" /></div>
+                            <div className="mb-2"><img className="img-fluid col-3" src={javascript} alt="" /></div>
                             <h3 className="h4 mb-2">Javascript</h3>
                             
                         </div>
                     </div>
                     <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
-                            <div className="mb-2"><i className="bi-laptop fs-1 text-primary"></i></div>
+                            <div className="mb-2"><img className="img-fluid col-3" src={react} alt="" /></div>
                             <h3 className="h4 mb-2">ReactJS</h3>
                             
                         </div>
                     </div>
                     <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
-                            <div className="mb-2"><i className="bi-globe fs-1 text-primary"></i></div>
+                            <div className="mb-2"><img className="img-fluid col-3" src={java} alt="" /></div>
                             <h3 className="h4 mb-2">Java</h3>
                             
                         </div>
                     </div>
                     <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
-                            <div className="mb-2"><i className="bi-heart fs-1 text-primary"></i></div>
+                            <div className="mb-2"><img className="img-fluid col-3" src={spring} alt="" /></div>
                             <h3 className="h4 mb-2">Spring</h3>
                             
                         </div>
                     </div>
                     <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
-                            <div className="mb-2"><i className="bi-heart fs-1 text-primary"></i></div>
+                            <div className="mb-2"><img className="img-fluid col-3" src={python} alt="" /></div>
                             <h3 className="h4 mb-2">Python</h3>
                             
                         </div>
                     </div>
                     <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
-                            <div className="mb-2"><i className="bi-heart fs-1 text-primary"></i></div>
+                            <div className="mb-2"><img className="img-fluid col-3" src={typescript} alt="" /></div>
                             <h3 className="h4 mb-2">Typescript</h3>
                             
                         </div>
                     </div>
                     <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
-                            <div className="mb-2"><i className="bi-heart fs-1 text-primary"></i></div>
+                            <div className="mb-2"><img className="img-fluid col-3" src={postegresql} alt="" /></div>
                             <h3 className="h4 mb-2">MySQL & PostgreSQL</h3>
                             
                         </div>
                     </div>
                     <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
-                            <div className="mb-2"><i className="bi-heart fs-1 text-primary"></i></div>
+                            <div className="mb-2"><img className="img-fluid col-3" src={s3} alt="" /></div>
                             <h3 className="h4 mb-2">AWS S3</h3>
                             
                         </div>
